c980532257a6015fa7ea359240335298888cae2b
Les Pounder <lespounder@gmail.com>
2015-12-04 13:57:55 +0000

    Code tested and loops correctly

--- Dr-Robot.py
+++ Dr-Robot.py
@@ -15,6 +15,7 @@ GPIO.setup(life2, GPIO.OUT)
 GPIO.setup(life3, GPIO.OUT)
 GPIO.setup(buzzer, GPIO.OUT)
 GPIO.setup(tool, GPIO.IN, GPIO.PUD_UP)
+GPIO.setup(button, GPIO.IN, GPIO.PUD_UP)
 
 def life_counter(lives):
     if lives == 3:
@@ -34,32 +35,30 @@ def life_counter(lives):
         GPIO.output(27,0)
         GPIO.output(22,0)
 
-try:
+lives = 3
+life_counter(lives)
+
+while True:
+    time.sleep(0.01)
     for i in range(2):
         GPIO.output(buzzer, 1)
         time.sleep(0.5)
         GPIO.output(buzzer,0)
         time.sleep(0.5)
-    lives = 3
-    life_counter(lives)
-    while True:
-        while lives > 0:
-            GPIO.wait_for_edge(9, GPIO.FALLING)
-            for i in range(3):
-                GPIO.output(buzzer, 1)
-                time.sleep(1)
-                GPIO.output(buzzer, 0)
-            time.sleep(1)
-            print("You lost a life")
-            lives = lives - 1
-            life_counter(lives)
-            if lives == 0:
-                break
-            break
-except KeyboardInterrupt:
-    print("Exit")
-        
-        
-            
-        
-        
+    while lives > 0:
+        time.sleep(0.01)
+        GPIO.wait_for_edge(tool, GPIO.FALLING)
+        for i in range(3):
+            GPIO.output(buzzer, 1)
+            time.sleep(0.2)
+            GPIO.output(buzzer, 0)
+            time.sleep(0.2)
+        time.sleep(0.1)
+        print("You lost a life")
+        lives = lives - 1
+        life_counter(lives)
+    if lives == 0:
+        print("Game Over")
+        time.sleep(3)
+        lives=3
+        life_counter(lives)
